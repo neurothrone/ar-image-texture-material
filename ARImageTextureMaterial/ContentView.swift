48c707a324dcb787455822a5070a7339b9d0ebda
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-15 17:43:16 +0200

    complete sample

--- ARImageTextureMaterial/ContentView.swift
+++ ARImageTextureMaterial/ContentView.swift
@@ -20,7 +20,56 @@ final class Coordinator {
   var cancellable: Cancellable?
   
   func setUp() {
+    guard let arView = arView else { return }
     
+    let anchor = AnchorEntity(plane: .horizontal)
+//    anchor.addChild(loadTextureResourceSynchronouslyAndAttachToAllFaces())
+    anchor.addChild(loadMultipleTextureResourcesAsynchronouslyAndAttachToEachFaceOfCube())
+    arView.scene.addAnchor(anchor)
+  }
+  
+  private func loadTextureResourceSynchronouslyAndAttachToAllFaces() -> ModelEntity {
+    let box = ModelEntity(mesh: .generateBox(size: 0.3))
+    let textureResource = try? TextureResource.load(named: "placeholder")
+    
+    if let textureResource {
+      var textureMaterial = UnlitMaterial()
+      textureMaterial.color = .init(tint: .white, texture: .init(textureResource))
+      box.model?.materials = [textureMaterial]
+    }
+    
+    return box
+  }
+  
+  private func loadMultipleTextureResourcesAsynchronouslyAndAttachToEachFaceOfCube() -> ModelEntity {
+    let box = ModelEntity(mesh: .generateBox(width: 0.3, height: 0.3, depth: 0.3, cornerRadius: .zero, splitFaces: true))
+    
+    cancellable = TextureResource.loadAsync(named: "placeholder")
+      .append(TextureResource.loadAsync(named: "cube"))
+      .append(TextureResource.loadAsync(named: "1.JPG"))
+      .append(TextureResource.loadAsync(named: "2.JPG"))
+      .append(TextureResource.loadAsync(named: "3.JPG"))
+      .append(TextureResource.loadAsync(named: "4"))
+      .collect()
+      .sink(receiveCompletion: { [weak self] completion in
+        if case let .failure(error) = completion {
+          fatalError("❌ -> Unable to load textures. Error: \(error)")
+        }
+        
+        self?.cancellable?.cancel()
+      }, receiveValue: { textureResources in
+        var materials: [UnlitMaterial] = []
+        
+        textureResources.forEach { texture in
+          var material = UnlitMaterial()
+          material.color = .init(tint: .white, texture: .init(texture))
+          materials.append(material)
+        }
+        
+        box.model?.materials = materials
+      })
+    
+    return box
   }
 }
 
